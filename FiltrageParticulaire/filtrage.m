50c85aa229959ff5d684fd9f52679b6ada822cd7
Adel-Larbi HARKAT <ah728511@zola.iem>
2023-02-24 09:51:41 +0100

    tp two almost done

---
+++ FiltrageParticulaire/filtrage.m
@@ -0,0 +1,20 @@
+function filtrage()
+	Config = chargeConfiguration();
+	Estimation = zeros(Config.nombre_de_frame,2) ;
+	Particules = initialisation(Config);
+	VT = chargeVeriteTerrain() ;
+	
+	for frame=1:Config.nombre_de_frame
+		I=chargeImage(Config,frame);
+        ParticulesAv=Particules;
+        
+		Particules = propagation(Particules,Config,Estimation,frame) ;
+      visuPropagation(ParticulesAv,Particules,Config,I,frame) ;
+		Particules = ponderation(Particules,Config,I) ;
+		[Estimation(frame,1) Estimation(frame,2)]=visuParticules(Particules,Config,I,frame);
+		Particules = resampling(Particules,Config) ;
+	end
+
+       display(Config.nombre_de_particule);
+	critere = evaluation(Config,Estimation,VT)
+
